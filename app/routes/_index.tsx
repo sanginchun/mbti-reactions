41e42df409bda42c173fc0ebf53771923845cec2
sanginchun <sanginchun91@gmail.com>
2023-04-23 12:16:03 +0900

    fix: index page styling, semantic tags

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -7,36 +7,33 @@ export const meta: V2_MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="max-w-lg h-screen mx-auto py-32 text-center font-mono bg-neutral-50">
+    <main className="max-w-lg h-screen mx-auto py-32 text-center font-mono bg-neutral-50">
       <h1 className="font-semibold text-4xl">MBTI별 반응😆</h1>
-      <div className="mt-20">
-        <Form
-          method="post"
-          action="/result"
-          autoComplete="off"
-          spellCheck="false"
+      <Form
+        method="post"
+        action="/result"
+        autoComplete="off"
+        spellCheck="false"
+        className="flex mt-20"
+      >
+        <p className="flex-1 px-3">
+          <input
+            required
+            maxLength={30}
+            placeholder="예) 오늘 기분이 별로여서 쇼핑했어"
+            type="text"
+            name="message"
+            className="w-full h-12 px-3 py-2 border rounded border-gray-200 caret-gray-500 placeholder:text-gray-300"
+          />
+        </p>
+        <button
+          type="submit"
+          className="w-14 mr-2 border border-transparent rounded bg-sky-500 text-white"
         >
-          <div className="flex">
-            <div className="flex-1 px-3">
-              <input
-                required
-                maxLength={30}
-                placeholder="예) 오늘 기분이 별로여서 쇼핑했어"
-                type="text"
-                name="message"
-                className="w-full h-12 p-3 border rounded border-gray-200 caret-gray-500 placeholder:text-gray-300"
-              />
-            </div>
-            <button
-              type="submit"
-              className="w-14 mr-2 border border-transparent rounded bg-sky-500 text-white"
-            >
-              전송
-            </button>
-          </div>
-        </Form>
-      </div>
-      <div className="mt-10 text-gray-400">최대 30자까지 입력할 수 있음</div>
-    </div>
+          전송
+        </button>
+      </Form>
+      <p className="mt-10 text-gray-400">최대 30자까지 입력할 수 있음</p>
+    </main>
   );
 }
