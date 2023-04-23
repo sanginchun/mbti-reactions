e19c2bb4b0f0c2db517087cc5a6a927f467ceaaa
sanginchun <sanginchun91@gmail.com>
2023-04-23 21:46:54 +0900

    fix: edit prompt

--- app/config/index.ts
+++ app/config/index.ts
@@ -1,4 +1,4 @@
 export const PROMPT = `Provide 16 reactions to the message given, each one for 16 MBTIs. Show the characteristics in their reactions, but keep the reaction natural. React like chit-chatting with a close friend. Each reaction should not exceed 10 words.
-Provide reactions in format below. Include all 16 personalities without duplication.
+Provide reactions in format below. Include all 16 personalities without duplication. Answer in same language with the user message.
 - ISTJ:
 - ISTP:`;
