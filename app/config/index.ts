064aaaebe6bbfa5040d6f2bd4240a72a26f138f7
sanginchun <sanginchun91@gmail.com>
2024-01-23 18:23:53 +0900

    fix: prompt 수정

--- app/config/index.ts
+++ app/config/index.ts
@@ -1,7 +1,8 @@
-export const PROMPT = `Provide 16 reactions to the message given, each one for 16 MBTIs. Show the characteristics in their reactions, but keep the reaction natural. React like chit-chatting with a close friend. Each reaction should not exceed 10 words.
-Provide reactions in format below. Include all 16 personalities without duplication. Reactions should be Korean language.
+export const PROMPT = `Provide 16 reactions to the message given, each one for 16 MBTI types. Show the characteristics in their reactions, keep the reaction natural, like talking to a close friend. Each reaction should not exceed 10 words. Include all 16 types without duplication. Follow the format below. Make sure to include hyphens in front of every type.
 - ISTJ:
-- ISTP:`;
+- ISTP:
+and so on
+`;
 
 // Provide 16 answers how would a person feel or think and act in the given situation, each one for 16 MBTI personalities. Show characteristics in the answers. Each answer should not exceed 10 words.
 
